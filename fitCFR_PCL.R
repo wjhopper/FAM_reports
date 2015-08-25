7eea6f13e2ed572c97250adb70bbc6e6130e7772
whopper-sculder <wjhopper510@gmail.com>
2015-08-25 15:24:47 -0400

    fitCFT_PCL is brought into the 21st century of model fitting routines

--- fitCFR_PCL.R
+++ fitCFR_PCL.R
@@ -1,81 +1,107 @@
+accuracyErrorFcn <- function(pars, fcn, fix, obs) {
+  if (!paramBounds(c(pars,fix))) {
+    return(1000000)
+  }
+  preds <- fcn(free=pars,fixed=fix,return_dist = FALSE)
+  obs <- obs %>% group_by(class,order) %>%
+    summarise(acc = sum(score)/4) %>%
+    ungroup() %>%
+    complete(class,order,fill=list(acc=0))
+  err <- sum((preds$acc-obs$acc^2))
+  return(err)
+}
+
+RT_ErrorFcn <- function(fcn, pars, fix, obs) {
+  if (!paramBounds(c(pars,fix))) {
+    return(1000000)
+  }
+  preds <- fcn(free=pars,fixed=fix,return_dist = TRUE)
+  err <- FAM:::LL(obs=obs[,c("class","order","RTrounded")],pred = preds$distribution)  
+  return(err)
+}
+
 fitCFR_PCL<- function(model,inpar = FALSE,...,debugLevel = 0) {
   library(optimx)
   library(dplyr)
   library(reshape2)
+  library(whoppeR)
+  library(tidyr)
+  
+  checkRequiredParams(c(model$free,model$fixed), model$fn)
+  
   if (inpar) {
-    packs <- requireNamespace(c('foreach', 'doParallel'), quietly = TRUE)
-    if (all(packs)) {
-      library(foreach)
-      library(doParallel)
-      cl <- makeCluster(detectCores()-1)
-      registerDoParallel(cl)
-      #     library(doRNG)
-      #     registerDoRNG(456)
-    } else {
-      warning("Packages foreach and doParallel not found to do parallel processing, falling back")
-      inpar = FALSE
-    }
+    cluster <- tryDoParallelCluster(detectCores()-1)
   }
   
-  checkRequiredParams(c(model$par,model$fixed), model$fn)
-  
-  if (inpar) {
+  if (inpar && cluster$success) {
     logfile <- tempfile("parlog", fileext = ".txt")
     writeLines(paste('[',Sys.time(),']',"INIT parlog"),
                con=logfile,sep='\n')
-    clusterExport(cl,"logfile",envir = environment())
+    clusterExport(cluster$handle,"logfile",envir = environment())
     model$results <- foreach(j =unique(model$data$subject), .verbose=T,
-                             .packages=c("optimx","PCL","whoppeR")) %dopar% {
+                             .packages=c("optimx","PCL","whoppeR","dplyr")) %dopar% {
       sink(logfile, append=TRUE)
       cat(paste("Fitting subject", j,"\n"))
       sink()
-      m  <- model
-      m$data <- model$data[model$data$subject == j,]
-      interim<- optimx(par=m$par[c("ER","LR")],fn=m$fn, method = m$method,
-                       itnmax = m$itnmax, data = m$data,
-                       fixed=c(m$fixed, m$par[!names(m$par) %in% c("ER","LR")]),
-                       fitAcc = TRUE,  fitRT= FALSE, fitting= m$fitting)
+      data <- model$data[model$data$subject == j,]
+      freePars <- names(model$free)
+      interim<- optimx(par=model$free[c("ER","LR")],
+                       fn = accuracyErrorFcn,
+                       method = model$method,
+                       control = list(maxit=1),
+                       fcn = model$fn,
+                       fix=c(model$fixed,
+                             model$free[!freePars %in% c("ER","LR")]),
+                       obs=data)
       newStart <- c(unlist(interim[,c("ER","LR")]), 
-                    m$par[!(names(m$par) %in% c("ER","LR"))])
-      fit <- optimx(par=newStart, fn=m$fn, method = m$method,
-                    itnmax = m$itnmax, fixed=m$fixed, data = m$data,
-                    fitting= m$fitting)      
+                    model$free[!freePars %in% c("ER","LR")])
+      fit <- optimx(par = newStart,
+                    fn = RT_ErrorFcn,
+                    method = m$method,
+                    control = list(maxit=1),
+                    fcn = model$fn,
+                    fix = model$fixed,
+                    obs = data)      
       best <- as.vector(coef(fit))
       names(best) <- colnames(coef(fit))
-      preds <- m$fn(free=best,fixed=m$fixed,data=m$data,
-                    fitting=FALSE)[c("preds","dist")]
+      preds <- model$fn(free = best,fixed = model$fixed)
       end <- list(fit=fit, preds = preds)
-      
-      }
+    }
     
-    stopCluster(cl)
+    stopCluster(cluster$handle)
     cat(paste('[',Sys.time(),']',"Finished Fitting, Goodbye"),
-        con=logfile,sep='\n')
+        file=logfile,sep='\n')
     
   } else {
     
-    model$results <- vector(mode = 'list', length = length(unique(model$IVdata$subject)))
-    for (j in 1:2){ #unique(model$data$subject)) {
+    model$results <- vector(mode = 'list', length = length(unique(model$data$subject)))
+    for (j in unique(model$data$subject)) {
       message(paste("Fitting subject", j))
-      m  <- model
-      m$data <- model$data[model$data$subject == j,]
-      interim<- optimx(par=m$par[c("ER","LR")],fn=m$fn, method = m$method,
-                       itnmax = m$itnmax, data = m$data,
-                       fixed=c(m$fixed, m$par[!names(m$par) %in% c("ER","LR")]),
-                       fitAcc = TRUE,  fitRT= FALSE, fitting= m$fitting)
+      data <- model$data[model$data$subject == j,]
+      freePars <- names(model$free)
+      interim<- optimx(par=model$free[c("ER","LR")],
+                       fn = accuracyErrorFcn,
+                       method = model$method,
+                       control = list(maxit=1),
+                       fcn = model$fn,
+                       fix=c(model$fixed,
+                             model$free[!freePars %in% c("ER","LR")]),
+                       obs=data)
       newStart <- c(unlist(interim[,c("ER","LR")]), 
-                    m$par[!(names(m$par) %in% c("ER","LR"))])
-      fit <- optimx(par=newStart, fn=m$fn, method = m$method,
-             itnmax = m$itnmax, fixed=m$fixed, data = m$data,
-             fitting= m$fitting)      
+                    model$free[!freePars %in% c("ER","LR")])
+      fit <- optimx(par = newStart,
+                    fn = RT_ErrorFcn,
+                    method = model$method,
+                    control = list(maxit=1),
+                    fcn = model$fn,
+                    fix = model$fixed,
+                    obs = data)      
       best <- as.vector(coef(fit))
       names(best) <- colnames(coef(fit))
-      preds <- m$fn(free=best,fixed=m$fixed,data=m$data,
-                    fitting=FALSE)[c("preds","dist")]
+      preds <- model$fn(free = best,fixed = model$fixed)
       model$results[[j]] <- list(fit=fit, preds = preds )
     }
   }
-
-
+  
   return(model)
 }
